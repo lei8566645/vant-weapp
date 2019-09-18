c9b401d7ef505e5155b63db285bb64c51591f826
大鹏 <dapeng@yujiahui.com>
2019-09-18 19:30:34 +0800

    fix(transition): custom-class不支持，需注意兼容

--- dist/icon/index.js
+++ dist/icon/index.js
@@ -2,7 +2,10 @@ import { VantComponent } from '../common/component';
 VantComponent({
     props: {
         info: null,
-        name: String,
+        name: {
+            type: String,
+            observer: 'nameIsSrc'
+        },
         size: String,
         color: String,
         customStyle: String,
@@ -11,9 +14,19 @@ VantComponent({
             value: 'van-icon'
         }
     },
+    data: {
+        isSrc: false
+    },
     methods: {
         onClick() {
             this.$emit('click');
+        },
+        nameIsSrc() {
+            const { name } = this.data;
+            const isSrc = name.indexOf('http') === 0 ||
+                name.indexOf('data:image') === 0 ||
+                name.indexOf('//') === 0;
+            this.set({ isSrc });
         }
     }
 });
--- packages/icon/index.ts
+++ packages/icon/index.ts
@@ -3,7 +3,10 @@ import { VantComponent } from '../common/component';
 VantComponent({
   props: {
     info: null,
-    name: String,
+    name: {
+      type: String,
+      observer: 'nameIsSrc'
+    },
     size: String,
     color: String,
     customStyle: String,
@@ -12,10 +15,20 @@ VantComponent({
       value: 'van-icon'
     }
   },
-
+  data: {
+    isSrc: false
+  },
   methods: {
     onClick() {
       this.$emit('click');
+    },
+    nameIsSrc() {
+      const { name } = this.data;
+      const isSrc =
+        name.indexOf('http') === 0 ||
+        name.indexOf('data:image') === 0 ||
+        name.indexOf('//') === 0;
+      this.set({ isSrc });
     }
   }
 });
